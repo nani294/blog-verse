c54d4699f8d94943ec0ff8477b97880518489d7d
nani294 <reddisharathkumar2006@gmail.com>
2025-09-06 12:37:18 +0530

    started nothing

--- Practice.js
+++ Practice.js
@@ -1,1 +1,33 @@
 console.log("Hello world, sai prasad")
+
+let name = "nani"
+console.log(name)
+
+let num=54
+console.log(num)
+console.log(typeof name)
+console.log(typeof num)
+
+let islogged=false
+console.log(islogged)
+console.log(typeof islogged)
+
+let x
+console.log(x)
+console.log(typeof x)
+
+let y=null;
+console.log(y)
+console.log(typeof y)
+
+let arr=[1,2,3,4,5,6]
+console.log(arr)
+console.log(typeof arr)
+
+let person={
+    name:"nani",
+    age:18
+}
+console.log(person.name)
+console.log(person.age)
+console.log(typeof person)
